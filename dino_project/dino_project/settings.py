9728097a66a02c993400a5e3b6e9e165a81878f0
Charlisteketee <charlisteketee@gmail.com>
2024-06-11 11:57:19 -0700

    Create axiosInstance and add authentication storage

--- dino_project/dino_project/settings.py
+++ dino_project/dino_project/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'photos',
     'users',
     'rest_framework',
+    'rest_framework.authtoken',
     'corsheaders',
 ]
 
@@ -82,6 +83,14 @@ WSGI_APPLICATION = 'dino_project.wsgi.application'
 
 AUTHENTICATION_BACKENDS = ['django.contrib.auth.backends.ModelBackend']
 
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': [
+        'rest_framework.authentication.TokenAuthentication',
+    ],
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.IsAuthenticated',
+    ],
+}
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
--- dino_project/users/views.py
+++ dino_project/users/views.py
@@ -1,8 +1,22 @@
 from rest_framework import status
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from django.contrib.auth import login, logout
 from .serializers import UserSerializer, LoginSerializer
+
+class CustomAuthToken(ObtainAuthToken):
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.serializer_class(data=request.data, context={'request':request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'username': user.pk,
+        })
 
 @api_view(['POST'])
 def register(request):
